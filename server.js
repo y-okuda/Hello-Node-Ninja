92bb084a8d1944091fe0fc879f30976b96a65db5
y-okuda <y-okuda@firstserver.co.jp>
2011-10-28 09:53:49 +0000

    fixed server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello Node Ninja & Yasuhiro-san\n');
+		res.end('Hello Node Ninja & Yasuhiro-kun\n');
 }).listen(8080);
